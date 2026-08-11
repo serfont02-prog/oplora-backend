70312ed6e3ee7ae4ba896cd45720aa234a2f41d7
serfont02-prog <serfont02@hotmail.com>
2026-08-11 19:35:15 +0200

    Corregir cors

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  app.enableCors({
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
-    credentials: true,
-  });
+app.enableCors({
+  origin: [
+    'http://localhost:3000',
+    'https://oplora-frontend.vercel.app',
+  ],
+  credentials: true,
+});
 
   await app.listen(process.env.PORT ?? 3001);
 }
